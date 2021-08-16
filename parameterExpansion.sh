232d56f2e9bc0f29d2740e9ba54306bb92f3e585
RoshanS21 <rosa00r@gmail.com>
2021-08-16 09:32:04 -0500

    parameter Expansion

---
+++ parameterExpansion.sh
@@ -0,0 +1,13 @@
+# Roshan Shrestha
+
+name="roshan"
+echo "${name}"
+echo "This is capitalized name"
+echo "${name^}"
+echo "This is uppercase name"
+echo "${name^^}"
+name=${name^^}
+echo "Lowecase the first character"
+echo "${name,}"
+echo "Lowercase everything"
+echo "${name,,}"
